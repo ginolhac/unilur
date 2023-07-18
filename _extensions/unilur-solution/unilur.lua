dcf58420c89fc52a4fd53b5767d960e9b3023ba4
Michael Lydeamore <michael.lydeamore@monash.edu>
2023-07-18 13:25:12 +1000

    Test turning into two extensions to provide output simultaneously

---
+++ _extensions/unilur-solution/unilur.lua
@@ -0,0 +1,55 @@
+-- Inspired from JJAlaire
+-- https://github.com/quarto-ext/code-filename/blob/main/_extensions/code-filename/code-filename.lua
+-- And Andrie
+-- https://github.com/andrie/reveal-auto-agenda/blob/main/_extensions/reveal-auto-agenda/reveal-auto-agenda.lua
+-- With precious help from Christophe Dervieux
+
+local options_solution = nil
+-- permitted options include:
+-- solution: true/false
+local function read_meta(meta)
+  local options = meta["show-solution"]
+  if options ~= nil then
+    options_solution = options
+  end
+end
+
+
+local function Div(el)
+  local options_collapse = true
+  if el.classes:includes("cell") and el.attributes["unilur-solution"] == "true" then
+    el.attributes["unilur-solution"] = nil
+    -- Embed solution code/block inside a callout if global option is true
+    if options_solution then
+      if quarto.doc.hasBootstrap() or quarto.doc.isFormat("revealjs") then
+        quarto.doc.addHtmlDependency({
+          name = "unilur",
+          version = "0.0.2",
+          stylesheets = {"unilur.css"}
+        })
+      end
+      -- collapse callout by default except specified
+      if el.attributes["unilur-collapse"] == "false" then
+        options_collapse = false
+      end
+      return {quarto.Callout({
+        content =  { el },
+        icon = false,
+        title = "Solution",
+        collapse = options_collapse,
+        type = "caution"
+        })}
+    else
+      return {} -- remove the solution chunks for questions
+    end
+  end
+end
+
+
+
+-- Run in two passes so we process metadata
+-- and then process the divs
+return {
+  {Meta = read_meta},
+  {Div = Div}
+}
